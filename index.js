b5acf033689831a7c218738bf612d2aeb8525c05
christian-bick <christian.bick@actano.de>
2016-10-04 09:09:53 +0200

    Move task-actions.

--- index.js
+++ index.js
@@ -7,20 +7,38 @@ if (!process.env.PORT) throw Error('PORT missing but required')
 var app = express();
 var slapp = Slapp({ context: BeepBoopContext() })
 
-slapp.message('^(hi|hello|hey).*', ['direct_mention', 'direct_message'], (msg, text, greeting) => {
-  msg
-      .say(`${greeting}, how are you?`)
-      .route('handleHowAreYou')  // where to route the next msg in the conversation
-})
-
-// register a route handler
-slapp.route('handleHowAreYou', (msg) => {
-  // respond with a random entry from array
-  msg.say(['Me too', 'Noted', 'That is interesting'])
-})
-
 const HelpActions = require('./lib/help-actions')
-const helpActions = new HelpActions()
+const TaskActions = require('./lib/task-actions')
+const TaskStore = require('./lib/task-store')
+const ConversationStore = require('./lib/conversation-store')
+
+store = {
+  tasks: new TaskStore(),
+  conversations: new ConversationStore()
+};
+
+const taskActions = new TaskActions(store);
+const helpActions = new HelpActions();
+
+/**
+ * Task commands
+ */
+slapp.command('/plan', taskActions.addTask);
+slapp.command('/tasks', taskActions.listTasks);
+
+/**
+ * Task actions
+ */
+slapp.action('task_action', 'remove', taskActions.removeTask);
+slapp.action('task_action', 'list_assign_options', taskActions.listAssignOptions);
+slapp.action('task_action', 'assign_self', taskActions.assignSelf);
+slapp.action('task_action', 'assign_any', taskActions.assignAny);
+slapp.action('task_action', 'cancel', taskActions.showDetails);
+slapp.action('task_action', 'details', taskActions.showDetails);
+slapp.action('task_action', 'list_status_options', taskActions.listStatusOptions);
+slapp.action('task_action', 'set_status', taskActions.setStatus);
+slapp.action('task_action', 'set_description', taskActions.setDescription);
+slapp.action('task_action', 'set_title', taskActions.setTitle);
 
 slapp.use((message, next) => {
   const event = message.body.event
@@ -36,6 +54,18 @@ slapp.use((message, next) => {
 
 slapp.command('/workstreams', helpActions.showHelp)
 slapp.action('help', 'show', helpActions.showHelp)
+
+/**
+ * Handle replies in conversations
+ */
+slapp.command('/reply', (message) => {
+  let conversation = store.conversations.get(message);
+  if (conversation) {
+    conversation(message);
+  } else {
+    console.log("Saw a repl without an open conversation")
+  }
+})
 
 // attach handlers to an Express app
 slapp.attachToExpress(app).listen(process.env.PORT)
